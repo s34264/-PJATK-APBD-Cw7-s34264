4d29d346067a4f50055e44c5539ead97c45f46a5
s34264 <s34264@pjwstk.edu.pl>
2026-05-21 01:30:58 +0200

    AppDbContext updated

---  PJATK-APBD-Cw7-s34264/Models/AppDbContext.cs
+++  PJATK-APBD-Cw7-s34264/Models/AppDbContext.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 namespace PJATK_APBD_Cw7_s34264.Models;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Component> Components { get; set; }
+    public DbSet<ComponentType> ComponentTypes { get; set; }
+    public DbSet<PCComponent> PCComponents { get; set; }
+    public DbSet<PC> PCs { get; set; }
+    public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
 }
